e44a39903aeb93f93fc71c05120ea6a6e89d9adc
Jason Qiu <jqiu21@protonmail.com>
2022-09-01 14:08:39 -0400

    Add first assignment

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Filename: helloworld.cpp
+ * Author  : Jason Qiu
+ * Version : 1.0
+ * Date    : September 1, 2022
+ * Description: Prints 'Hello world!' to stdout.
+ * Pledge  : I pledge my honor that I have abided by the Stevens Honor System.
+ ******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl;
+    return 0;
+}
